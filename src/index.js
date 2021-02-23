b2f17a0c9ff43522b6fb513d108f9c2c72608e37
Eugene B <diemorgenstern@gmail.com>
2021-02-22 22:26:21 -0500

    init: solve solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array||array.length === 0) 
+    return 0;
+  if (array.length === 1)
+    return array;
+  let minValue = array[0];
+  array.forEach(element => {
+    minValue = (element < minValue) ? element : minValue ;
+  });
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array||array.length === 0) 
+    return 0;
+  if (array.length === 1)
+    return array;
+  let maxValue = array[0];
+  array.forEach(element => {
+    maxValue = (element > maxValue) ? element : maxValue ;
+  });
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array||array.length === 0) 
+    return 0;
+  if (array.length === 1)
+    return array;
+  let sum = 0;
+  array.forEach(element => {
+    sum += element;
+  });
+  return (sum / array.length);
 }
